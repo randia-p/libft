22dd1037057d9ab43431dbd1c7962926cca75b9f
randia-p <60110630+randia-p@users.noreply.github.com>
2020-10-01 15:00:33 +0200

    last

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: randia-p <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/13 12:49:42 by abello-r          #+#    #+#             */
+/*   Updated: 2020/01/13 14:41:14 by abello-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void		ft_putchar(char x, int fd)
+{
+	write(fd, &x, 1);
+}
+
+void			ft_putnbr_fd(long n, int fd)
+{
+	int long i;
+
+	i = n;
+	if (i < 0)
+		i = i * -1;
+	if (i > 9)
+		ft_putnbr_fd(i / 10, fd);
+	ft_putchar(i % 10 + 48, fd);
+}
